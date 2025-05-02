56ec0f8701557da5da75ed8320dbf2d892e90a41
hirokihello <iammyeye1@gmail.com>
2025-05-02 20:01:55 +0900

    fix

--- app/main.go
+++ app/main.go
@@ -56,7 +56,7 @@ func main() {
 			} else if x == '/' {
 				fmt.Println("SLASH / null")
 			} else if x == '=' {
-				if string(fileContents[i+1]) == "=" {
+				if fileContents[i+1] == '=' {
 					fmt.Println("EQUAL_EQUAL == null")
 				} else {
 					fmt.Println("EQUAL = null")
