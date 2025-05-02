729edaa407377758cb01c86a9fcf81663783d83e
hirokihello <iammyeye1@gmail.com>
2025-05-02 19:49:32 +0900

    fix: 標準エラー出力に出力するように修正

--- app/main.go
+++ app/main.go
@@ -56,7 +56,8 @@ func main() {
 			} else if x == '/' {
 				fmt.Println("SLASH / null")
 			} else {
-				fmt.Errorf("[line 1] Error: Unexpected character: %c\n", x)
+				fmt.Fprintf(os.Stderr, "[line 1] Error: Unexpected character: %c\n", x)
+
 				errCount++
 			}
 		}
